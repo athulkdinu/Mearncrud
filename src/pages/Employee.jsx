18f122bc9c4ce7399f2d159837f403634bf86a20
athulkdinu <athulkdinu@gmail.com>
2025-12-30 06:54:57 +0530

    final

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Modal from 'react-bootstrap/Modal';
+import { addEmpAPI, deleteEmpAPI, getAEmpAPI, getAllEmpAPI, updateEmpAPI } from '../services/AllAPI';
+import { useEffect } from 'react';
 
 
 function Employee() {
@@ -10,9 +12,16 @@ function Employee() {
   const[empData,setEmpData]=useState({
     empname:"",
     empDesg:"",
-    empsalary:""
+    empSalary:""
   })
   console.log(empData);
+  const [allEmployees, setAllEmployees] = useState([]);
+const [editEmpData, setEditEmpData] = useState({
+  empname: "",
+  empDesg: "",
+  empSalary: ""
+});
+
   
 
   const handleClose = () => setShow(false);
@@ -20,32 +29,117 @@ function Employee() {
   const handleClose2 = () => setShow2(false);
   const handleShow2 = () => setShow2(true);
 
+  // add employee
+   
+  const addEmployee = async ()=>{
+    try {
+      const result =await addEmpAPI(empData)
+      console.log(result);
+        getAllEmployee()        
+    handleClose()  
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
 
+  //get all employee
 
+  const getAllEmployee = async ()=>{
+    try {
+      const Allemp =await getAllEmpAPI()
+      setAllEmployees(Allemp.data);    
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+  //get a employee
+
+  const getAEmployee = async(id)=>{
+   try {
+      const result = await getAEmpAPI(id)
+      console.log(result);  
+     setEditEmpData(result.data);
+     setShow2(true);   
+    } catch (error) {
+      console.log(error);     
+   }
+  }
+console.log(editEmpData);
+
+//edit emp
+
+const updateEmployee= async()=>{
+  try {
+    const result =await updateEmpAPI(editEmpData)
+    console.log(result);
+    getAllEmployee()     // refresh list
+    handleClose2()
+    
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+//delete
+
+const handledelete =async(id)=>{
+  console.log(id);
+  try {
+    const result =await deleteEmpAPI(id)
+    console.log(result);
+    getAllEmployee()
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
   
+}
+
+
+useEffect(() => {
+  getAllEmployee();
+}, []);
+
   return (
     <>
-      <div clasName='container mt-5'>
+      <div className='container mt-5'>
         <div className='w-100 d-flex justify-content-end'>
           <button onClick={handleShow} className='btn btn-success '>Add Emplpoyee</button>
         </div>
       </div>
-      <div className="row mt-5">
-        <div className="col-4 p-3 shadow rounded">
-          <Card style={{ width: '100%' }}>
-            <Card.Body>
-              <p className='fs-4'>Employee Name:Athul</p>
-              <p className='fs-4'>Designation:Developer</p>
-              <p className='fs-4'>Salary:500000</p>
-            </Card.Body>
-            <div className='d-flex justify-content-around p-3'>
-              <button onClick={handleShow2} className='btn btn-warning'>Edit</button>
-              <button className='btn btn-danger'>Delete</button>
-
-            </div>
-          </Card>
+     <div className="row mt-5">
+  {allEmployees?.map((emp) => (
+    <div key={emp._id} className="col-4 p-3">
+      <Card style={{ width: '100%' }} className="shadow rounded">
+        <Card.Body>
+          <p className='fs-4'>Employee Name: {emp.empname}</p>
+          <p className='fs-4'>Designation: {emp.empDesg}</p>
+          <p className='fs-4'>Salary: {emp.empSalary}</p>
+        </Card.Body>
+        <div className='d-flex justify-content-around p-3'>
+          <button
+            onClick={() => {
+              // handleShow2();
+              getAEmployee(emp._id);
+            }}
+            className='btn btn-warning'
+          >
+            Edit
+          </button>
+          <button onClick={()=>handledelete(emp?._id)} className='btn btn-danger'>Delete</button>
         </div>
-      </div>
+      </Card>
+    </div>
+  ))}
+</div>
+
       {/*modal*/}
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
@@ -58,11 +152,11 @@ function Employee() {
         </div>
           <div className='mb-3'>
             <label htmlFor="">Employee Designation</label>
-             <input value={empData.empDesg} type="text" onChange={(e)=>setEmpData({...empData,empname:e.target.value})}  placeholder='employee Designation'className='form-control'/>
+             <input value={empData.empDesg} type="text" onChange={(e)=>setEmpData({...empData,empDesg:e.target.value})}  placeholder='employee Designation'className='form-control'/>
         </div>
           <div className='mb-3'>
             <label htmlFor="">Employee Salary</label>
-             <input value={empData.empsalary} type="text" onChange={(e)=>setEmpData({...empData,empname:e.target.value})} placeholder='employee Salary'className='form-control'/>
+             <input value={empData.empSalary} type="text" onChange={(e)=>setEmpData({...empData,empSalary:e.target.value})} placeholder='employee Salary'className='form-control'/>
         </div>
 
         </Modal.Body>
@@ -70,7 +164,7 @@ function Employee() {
           <Button variant="secondary" onClick={handleClose}>
             Clear
           </Button>
-          <Button variant="primary" >
+          <Button onClick={addEmployee} variant="primary" >
             Add Employee
           </Button>
         </Modal.Footer>
@@ -84,23 +178,23 @@ function Employee() {
         <Modal.Body>
         <div className='mb-3'>
             <label htmlFor="">Employee Name</label>
-             <input type="text"  placeholder='employee name' className='form-control'/>
+             <input value={editEmpData.empname} onChange={(e) =>setEditEmpData({ ...editEmpData, empname: e.target.value })} type="text"  placeholder='employee name' className='form-control'/>
         </div>
           <div className='mb-3'>
             <label htmlFor="">Employee Designation</label>
-             <input type="text"  placeholder='employee Designation'className='form-control'/>
+             <input value={editEmpData.empDesg}  onChange={(e) =>setEditEmpData({ ...editEmpData, empDesg: e.target.value })} type="text"  placeholder='employee Designation'className='form-control'/>
         </div>
           <div className='mb-3'>
             <label htmlFor="">Employee Salary</label>
-             <input type="text"  placeholder='employee Salary'className='form-control'/>
+             <input value={editEmpData.empSalary}  onChange={(e) =>setEditEmpData({ ...editEmpData, empSalary: e.target.value })}  type="text"  placeholder='employee Salary'className='form-control'/>
         </div>
 
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={handleClose2}>
             Reset
           </Button>
-          <Button variant="primary" >
+          <Button onClick={updateEmployee} variant="primary" >
             update Employee
           </Button>
         </Modal.Footer>
--- src/services/AllAPI.js
+++ src/services/AllAPI.js
@@ -4,18 +4,30 @@ import serverURL from "./serverURL"
 
 //add employee
 
-export const addEmpAPI =async()=>{
+export const addEmpAPI =async(reqBody)=>{
     return await commonAPI("POST",`${serverURL}/addemp`,reqBody,{})
 }
 
 //get all employee
 
-// export const getAllEmpAPI = async()=>{
-//     return await commonAPI("GET",`${serverURL}/geta`)
-// }
+export const getAllEmpAPI = async()=>{
+    return await commonAPI("GET",`${serverURL}/all-employees`,"",{})
+}
 
 //get a employee
+export const getAEmpAPI = async(id)=>{
+    return await commonAPI("GET",`${serverURL}/get-emp/${id}`,{})
+}
+
 
 //update
+export const updateEmpAPI = async(reqBody)=>{
+    return await commonAPI("PUT",`${serverURL}/update-emp`,reqBody)
+}
+
+
 
 //delete emp
+export const deleteEmpAPI = async(id)=>{
+    return await commonAPI("delete",`${serverURL}/remove-emp/${id}`)
+}
